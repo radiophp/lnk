8b6dafc48c2b6e9eb39973e862ea68d19e9f8ef1
radiophp <radiophp@gmail.com>
2022-06-24 20:06:16 +0300

    last commit

--- src/Controller/CircleController.php
+++ src/Controller/CircleController.php
@@ -11,15 +11,15 @@ use Symfony\Component\HttpFoundation\Request;
 class CircleController extends AbstractController
 {
     #[Route('/circle/{radius<\d+>}', name: 'app_circle')]
-    public function index(Circle $Circle,Request $request): JsonResponse
+    public function index(Circle $Circle, Request $request): JsonResponse
     {
 
-        $Circle->setRadius( $request->attributes->getDigits('radius') );
+        $Circle->setRadius($request->attributes->getDigits('radius'));
         $return_array = [
-            'type'          =>  'circle',
-            'radius'        =>  $Circle->getRadius(),
-            'surface'       =>  $Circle->getSurface(),
-            'circumference' =>  $Circle->getCircumference()
+            'type' => 'circle',
+            'radius' => $Circle->getRadius(),
+            'surface' => $Circle->getSurface(),
+            'circumference' => $Circle->getCircumference()
         ];
         return $this->json($return_array);
     }
--- src/Controller/GeometryController.php
+++ src/Controller/GeometryController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,28 +13,20 @@ use App\Service\GeometryCalculator;
 
 class GeometryController extends AbstractController
 {
-    #[Route('/geometry', name: 'app_geometry' ,methods: 'GET'  )]
-    public function index(Request $request ): JsonResponse
+    #[Route('/geometry', name: 'app_geometry', methods: 'GET')]
+    public function index(Request $request): JsonResponse
     {
-
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/GeometryController.php',
-        ]);
+        return $this->json(['message' => 'Welcome to your geometry']);
     }
 
-    #[Route('/geometry/{shapeMode}', name: 'app_geometry_new' ,methods: 'POST'  )]
-    public function Sum(Request $request ,GeometryCalculator $GC): Response
+    #[Route('/geometry/{shapeMode}', name: 'app_geometry_new', methods: 'POST')]
+    public function Sum(Request $request, GeometryCalculator $GC): Response
     {
         $parameters = json_decode($request->getContent(), true);
         $reqType = $request->attributes->get('shapeMode');
-
-        return $this->json( $GC->calculator($parameters,$reqType) );
+        if (!in_array($reqType, ['surface', 'circumference'])) {
+            throw new Exception('Invalid Request url : ' . $reqType);
+        }
+        return $this->json($GC->calculator($parameters, $reqType));
     }
-   /* #[Route('/geometry/surface_sum', name: 'app_geometry_new' ,methods: 'POST'  )]
-    public function surfaceSum(Request $request ,GeometryCalculator $GC): Response
-    {
-        $parameters = json_decode($request->getContent(), true);
-        return $this->json( $GC->calculator($parameters,'surface') );
-    }*/
 }
