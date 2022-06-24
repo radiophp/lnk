0cca00f9d904eecbf005091063550dd6e403883f
radiophp <radiophp@gmail.com>
2022-06-24 19:33:09 +0300

    final

--- src/Controller/GeometryController.php
+++ src/Controller/GeometryController.php
@@ -6,17 +6,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use App\Service\GeometryCalculator;
+
 
 class GeometryController extends AbstractController
 {
-    #[Route('/geometry', name: 'app_geometry' ,methods: 'POST'  )]
-    public function index(Request $request): JsonResponse
+    #[Route('/geometry', name: 'app_geometry' ,methods: 'GET'  )]
+    public function index(Request $request ): JsonResponse
     {
-        var_dump($request->get('type'));
-        die("asdas");
+
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/GeometryController.php',
         ]);
     }
+
+    #[Route('/geometry/{shapeMode}', name: 'app_geometry_new' ,methods: 'POST'  )]
+    public function Sum(Request $request ,GeometryCalculator $GC): Response
+    {
+        $parameters = json_decode($request->getContent(), true);
+        $reqType = $request->attributes->get('shapeMode');
+
+        return $this->json( $GC->calculator($parameters,$reqType) );
+    }
+   /* #[Route('/geometry/surface_sum', name: 'app_geometry_new' ,methods: 'POST'  )]
+    public function surfaceSum(Request $request ,GeometryCalculator $GC): Response
+    {
+        $parameters = json_decode($request->getContent(), true);
+        return $this->json( $GC->calculator($parameters,'surface') );
+    }*/
 }
--- src/Service/GeometryCalculator.php
+++ src/Service/GeometryCalculator.php
@@ -1,8 +1,43 @@
 <?php
 
 namespace App\Service;
+use App\Service\Circle;
+use App\Service\Triangle;
+use JetBrains\PhpStorm\ArrayShape;
 
 class GeometryCalculator
 {
+    var $circle;
+    var $triangle;
+    function __construct(Circle $circle,Triangle $triangle) {
+        $this->circle =$circle;
+        $this->triangle=$triangle;        ;
+    }
 
+    #[ArrayShape(['sum' => "mixed", 'errorCount' => "int"])]
+    public function calculator(array $parameters, string $mode): array
+    {
+        $mode = ucfirst($mode);
+        $shapeTypes = ['circle','triangle'];
+        $areaSum = 0;
+        $errorCount = 0;
+        foreach($parameters as $shape)
+        {
+            if(strtolower($shape['type']) =='circle') {
+                $this->circle->setRadius($shape['radius']);
+                $areaSum += $this->circle->{'get'.$mode}();
+            }
+            elseif(strtolower($shape['type']) =='triangle') {
+                $this->triangle->setSides($shape['a'],$shape['b'],$shape['c']);
+                $areaSum += $this->triangle->{'get'.$mode}();
+            }
+            else {
+                $errorCount += 1;
+            }
+        }
+        return  [
+                    'sum'        => $areaSum,
+                    'errorCount' => $errorCount,
+                ];
+    }
 }
